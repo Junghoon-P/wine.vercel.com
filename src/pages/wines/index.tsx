186d0a75f0b885f354ffd7aefe724ef82dccdc22
Junghoon-P <qkr0454@naver.com>
2021-12-22 00:04:09 +0900

    ♻️ component-refactoring

--- src/pages/wines/index.tsx
+++ src/pages/wines/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const WinePage: NextPage = () => {
   return (
     <div>
-      <h1>Wine</h1>
+      <h1>Wines</h1>
     </div>
   );
 };
--- src/pages/wines/rose.tsx
+++ src/pages/wines/rose.tsx
@@ -1,9 +1,25 @@
 import type { NextPage } from "next";
+import { useWineData } from "../../hooks/useWineData";
+import { Wine } from "../../types/Wine";
+import { WineCard, Error, Loading } from "../../components";
 
 const Rose: NextPage = () => {
+  const name = "rose";
+  //useSwr은 hooks이기 때문에 CSR에서만 가능하다. SSR 불가
+  const { data, error } = useWineData(name);
+
+  if (error) return <Error />;
+  if (!data) return <Loading />;
+
   return (
     <div>
       <h1>Rose</h1>
+      <main>
+        {data.map((wineData: Wine) => {
+          const { id } = wineData;
+          return <WineCard key={`port-wine-list-${id}`} wineData={wineData} />;
+        })}
+      </main>
     </div>
   );
 };
