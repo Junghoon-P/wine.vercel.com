b61c3da2389f3ab9851c0cf07edf92afbbe218d7
Junghoon-P <qkr0454@naver.com>
2021-12-23 19:51:41 +0900

    💄WineCardList styled

--- src/components/WineCardList.tsx
+++ src/components/WineCardList.tsx
@@ -1,6 +1,8 @@
 import { useWineData } from "../hooks";
 import { Wine } from "../types";
 import { WineCard, Error, Loading } from "../components";
+import styled from "@emotion/styled";
+import { MEDIA_QUERY_END_POINT } from "../constants";
 
 interface WineCardListProps {
   name: string;
@@ -14,17 +16,33 @@ const WineCardList = ({ name }: WineCardListProps) => {
 
   return (
     <div>
-      <h1>Port</h1>
-      <main>
+      <WineTitle>Port</WineTitle>
+      <WineCardContainer>
         {data.map((wineData: Wine) => {
           const { id } = wineData;
           return (
             <WineCard key={`${name}-wine-list-${id}`} wineData={wineData} />
           );
         })}
-      </main>
+      </WineCardContainer>
     </div>
   );
 };
 
 export default WineCardList;
+
+const WineTitle = styled.h1`
+  line-height: 180%;
+`;
+
+const WineCardContainer = styled.main`
+  display: grid;
+
+  @media (min-width: ${MEDIA_QUERY_END_POINT.MOBILE}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: ${MEDIA_QUERY_END_POINT.TABLET}) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
