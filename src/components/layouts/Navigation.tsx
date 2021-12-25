27b530bbcfec68fbe84e83adf67af9594815b516
Junghoon-P <qkr0454@naver.com>
2021-12-25 23:11:00 +0900

    :recycle: Refactoring & remove unused pakage
    setting nav & remove styled-components

--- src/components/layouts/Navigation.tsx
+++ src/components/layouts/Navigation.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import Link from "next/link";
 import { ROUTES } from "../../constants";
 
@@ -10,17 +11,17 @@ interface ROUTE {
 
 export const Navigation = () => {
   return (
-    <header>
-      <h1>Wine and Beer</h1>
-      <nav>
-        <ul>
+    <NavHeader>
+      <Header>Life is YO-G-Gyung</Header>
+      <MainCategoryWrap>
+        <MainCategory>
           {ROUTES.map((routeObject: ROUTE) => {
             return (
               <li key={routeObject.LABEL}>
                 <Link href={routeObject.PATH}>
                   <a>{routeObject.LABEL}</a>
                 </Link>
-                <ul>
+                <SubCategory id="kid">
                   {routeObject.SUBS &&
                     routeObject.SUBS.map((subRouteObject: ROUTE) => {
                       return (
@@ -33,12 +34,49 @@ export const Navigation = () => {
                         </li>
                       );
                     })}
-                </ul>
+                </SubCategory>
               </li>
             );
           })}
-        </ul>
-      </nav>
-    </header>
+        </MainCategory>
+      </MainCategoryWrap>
+    </NavHeader>
   );
 };
+
+const NavHeader = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100vw;
+`;
+
+const Header = styled.h1`
+  font-size: 30px;
+  cursor: pointer;
+`;
+
+const MainCategoryWrap = styled.div`
+  display: flex;
+`;
+
+const MainCategory = styled.ul`
+  position: relative;
+  display: flex;
+  justify-content: space-around;
+  width: 200px;
+  font-size: 20px;
+  font-weight: 600;
+
+  &:hover #kid {
+    display: block;
+    width: 100%;
+    color: red;
+  }
+`;
+
+const SubCategory = styled.ul`
+  display: none;
+  position: absolute;
+`;
